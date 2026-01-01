a1dd51490ee663187f8288c635933e37529f095f
Tomer Ramon <tomer.ramon1@gmail.com>
2026-01-01 10:22:59 +0200

    rename files

--- bank_backend/src/app.js
+++ bank_backend/src/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const app = express();
 
-const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/user");
-const transactionRoutes = require("./routes/transaction");
+const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
+const transactionRoutes = require("./routes/transaction.routes");
 
 
 // Middleware
