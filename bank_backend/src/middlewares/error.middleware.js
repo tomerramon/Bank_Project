0491d730eeb3936217c470b3fa0352916b8a0bfb
Tomer Ramon <tomer.ramon1@gmail.com>
2026-02-15 15:37:00 +0200

    refactor: enhance error logging in errorHandler middleware

--- bank_backend/src/middlewares/error.middleware.js
+++ bank_backend/src/middlewares/error.middleware.js
@@ -1,9 +1,13 @@
 import { errorToResponse, getStatusCode } from "../utils/errors.util.js";
 
 export function errorHandler(err, req, res, next) {
-	console.error("Unhandled error:", err);
+	console.error(`Error on ${req.method} ${req.path}:`, err.message);
 
 	const statusCode = getStatusCode(err);
+
+	if (res.headersSent) {
+		return next(err);
+	}
 
 	res.status(statusCode).json(
 		errorToResponse(err, process.env.NODE_ENV === "development"),
